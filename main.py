79b44dd9faff2cd171f13f0228032840155ee147
Arnav Bansal <arnav.bansal95@gmail.com>
2023-10-24 19:20:28 +0200

    clean up

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotly.figure_factory as ff
 from collections import OrderedDict
 
 # Task_Set = [t(C, T, P)] :-> C - ExecutionTime, T - TimePeriod, P - Priority (1 - 255)
-task_set = [(1, 5, 1), (1, 10, 2), (1, 15, 3), (1, 20, 4)]
+task_set = [(1, 5, 1), (1, 10, 2), (1, 15, 3)]
 
 
 def find_scheduling_points(input_task_set):
@@ -57,28 +57,17 @@ def create_schedule_table_gantt_chart(input_task_set, input_schedule_table):
     df = pd.DataFrame(dataframe_list)
     fig = ff.create_gantt(df, index_col='Resource', bar_width=0.4, show_colorbar=True, title="Schedule Table",
                           group_tasks=True)
-    fig.update_layout(xaxis_type='linear', autosize=False, width=1800, height=450)
+    fig.update_layout(xaxis_type='linear', autosize=False, width=1200, height=350)
     fig.update_xaxes(showgrid=False, showline=True, linewidth=2, linecolor='black')
     for point in input_schedule_table:
         fig.add_vline(x=int(point), line_width=2, line_color="black")
-    fig.show()
-
-    '''df = pd.DataFrame([
-        dict(Task="Task 1", Start=2, Finish=3, Resource="T1"),
-        dict(Task="Task 2", Start=1, Finish=2, Resource="T2"),
-        dict(Task="Task 3", Start=0, Finish=1, Resource="T3"),
-        dict(Task="Task 3", Start=3, Finish=4, Resource="T3")
-    ])
-
-    fig = ff.create_gantt(df, index_col='Resource', bar_width=0.4, show_colorbar=True, title="Schedule Table",
-                          group_tasks=True)
-    fig.update_layout(xaxis_type='linear', autosize=False, width=800, height=400)
-    fig.show()'''
+    fig.write_html('first_figure.html', auto_open=True)
 
 
 if __name__ == '__main__':
     schedule_table = find_scheduling_points(input_task_set=task_set)
     find_schedule_table(input_task_set=task_set, input_scheduling_points=schedule_table)
-    print(schedule_table)
+    for point in schedule_table:
+        print(str(point) + ":\t" + str(schedule_table[point]))
 
     create_schedule_table_gantt_chart(input_task_set=task_set, input_schedule_table=schedule_table)
